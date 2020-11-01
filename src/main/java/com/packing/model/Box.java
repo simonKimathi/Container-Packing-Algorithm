673b32f1e98b08ceb973f656d61470d5eb67e9b8
symoh <smnkimathi@gmail.com>
2020-11-01 22:23:00 +0300

    Minor fixes

--- src/main/java/com/packing/model/Box.java
+++ src/main/java/com/packing/model/Box.java
@@ -1,44 +1,14 @@
 package com.packing.model;
 
-import java.io.Serializable;
 import java.util.Objects;
 
-public class Box implements Serializable {
-     private double height;
-     private double width;
-     private double length;
+public class Box extends Shape {
 
     public Box() {
     }
 
     public Box(double height, double width, double length) {
-        this.height = height;
-        this.width = width;
-        this.length = length;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
+        super(height, width, length);
     }
 
     @Override
@@ -59,9 +29,9 @@ public class Box implements Serializable {
     @Override
     public String toString() {
         return "Box{" +
-                "height=" + height +
-                ", width=" + width +
-                ", length=" + length +
+                "height=" + this.getHeight() +
+                ", width=" + this.getWidth() +
+                ", length=" + this.getLength() +
                 '}';
     }
 }
